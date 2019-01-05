7ad11e1d0239f22f5d99b40f6d8645e6fba29e8c
psubrownie <scott.lusebrink@gmail.com>
2019-01-05 12:10:20 -0500

    Update blink.js

--- blink.js
+++ blink.js
@@ -17,30 +17,47 @@ myEmitter.on('AChase Complete', () => {
 });
 
 myEmitter.on('BChase Complete', () => {
-  setTimeout(runAChase,1000);
+               on();
+               setTimeout(function(){off();dualUP();},10000);
 });
 myEmitter.on('dualUp Complete',() => {
   dualDown();
 });
-//runAChase();
+myEmitter.on('dualDown Complete',() => {
+   on();
+   setTimeout(function(){off();runAChase();},10000);
+});
+runAChase();
 //dualUP()
-oddEven();
+//oddEven();
 
+function on(cb){
+  for(var x=0;x<8;x++){
+        wpi.digitalWrite(x,1);
+  }
+myEmitter.emit('On');
+}
+
+function off(){
+  for(var x=0;x<8;x++){
+    wpi.digitalWrite(x,0);
+  }
+}
 function runAChase(cb){
   var aSpeed = speed;
   var pin = -1;
   var oldpin;
   var pid = setInterval(chase, aSpeed);
-  
+
   function chase() {
     oldpin=pin++;
     wpi.digitalWrite(pin, 1);
     wpi.digitalWrite(oldpin,0);;
     if(pin>7){
-  	  pin=-1;
-       clearInterval(pid);    
-       myEmitter.emit('AChase Complete');   
-    }  
+          pin=-1;
+       clearInterval(pid);
+       myEmitter.emit('AChase Complete');
+    }
   }
 }
 function runBChase(){
@@ -48,16 +65,16 @@ function runBChase(){
   var bSpeed = speed;
   var oldpin;
   var pid = setInterval(chase, bSpeed);
-  
+
   function chase() {
     oldpin=pin--;
     wpi.digitalWrite(pin, 1);
     wpi.digitalWrite(oldpin,0);;
     if(pin<0){
-  	   pin=8;
+           pin=8;
        clearInterval(pid);
-       myEmitter.emit('BChase Complete');       
-    }  
+       myEmitter.emit('BChase Complete');
+    }
   }
 }
 
@@ -65,42 +82,42 @@ function dualUP(){
   var pinx =0;
   var piny =7;
   setIntervalX(function(){
-		 wpi.digitalWrite(pinx++,1);
-		 wpi.digitalWrite(pinx-2,0);
-		 wpi.digitalWrite(piny--,1);
-		 wpi.digitalWrite(piny+2,0);
-	       },speed,4,'dualUp Complete');
+                 wpi.digitalWrite(pinx++,1);
+                 wpi.digitalWrite(pinx-2,0);
+                 wpi.digitalWrite(piny--,1);
+                 wpi.digitalWrite(piny+2,0);
+               },speed,4,'dualUp Complete');
 }
 function dualDown(){
   var pinx =4-1;
   var piny =5-1;
   var oldx,oldy;
   setIntervalX(function(){
-		 oldx=pinx++;
-		 oldy=piny--;
-		 wpi.digitalWrite(pinx,1);
-		 wpi.digitalWrite(oldx,0);
-		 wpi.digitalWrite(piny,1);
-		 wpi.digitalWrite(oldy,0);
-	       },speed,5,'dualDown Complete');
+                 oldx=pinx++;
+                 oldy=piny--;
+                 wpi.digitalWrite(pinx,1);
+                 wpi.digitalWrite(oldx,0);
+                 wpi.digitalWrite(piny,1);
+                 wpi.digitalWrite(oldy,0);
+               },speed,5,'dualDown Complete');
 }
 
 function oddEven(){
   console.log('oddEven');
   var value = true;
   setIntervalX(function(){console.log('oddEven Loop');
-		 for(var x=0;x<8;x++){		   
-		   if((x+2)%2==0){//stupid 0 index
-		     wpi.digitalWrite(x,value?1:0);		 
-		     console.log('---'+x+':'+value?'1':'0');
-		   }
-		   else{
-		     wpi.digitalWrite(x,!value?1:0)
-		        console.log('---'+x+':'+!value?'1':'0')
-		       }
-		 }
-		 value = !value;
-	       },2000,4,'oddEven Complete');
+                 for(var x=0;x<8;x++){
+                   if((x+2)%2==0){//stupid 0 index
+                     wpi.digitalWrite(x,value?1:0);
+                     console.log('---'+x+':'+value?'1':'0');
+                   }
+                   else{
+                     wpi.digitalWrite(x,!value?1:0)
+                        console.log('---'+x+':'+!value?'1':'0')
+                       }
+                 }
+                 value = !value;
+               },200,4,'oddEven Complete');
 }
 
 function setIntervalX(callback, delay, repetitions,emits) {
@@ -111,7 +128,7 @@ function setIntervalX(callback, delay, repetitions,emits) {
        ;
        if (++x === repetitions) {
            clearInterval(intervalID);
-	   if(emits)myEmitter.emit(emits);
+           if(emits)myEmitter.emit(emits);
        }
     }, delay);
 }
